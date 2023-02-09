b5b8001e42db1043e33fe7e68b16f7877a8ff4f4
Kim Hyun Sung <kimhsr@naver.com>
2023-02-09 15:39:50 +0900

    feat: Banner Swiper 기능 구현 (#5)

--- apps/client/src/components/Banner.tsx
+++ apps/client/src/components/Banner.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styled from 'styled-components';
 
-const BaContainer = styled.div`
+import { Navigation, Pagination, Autoplay } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+const BaContainer = styled.div<{ bg: string }>`
   position: relative;
-  top: 70px;
-  background-color: skyblue;
+  top: 25px;
+  background-color: ${(props) => props.bg};
   width: 100%;
   max-width: 71.25rem;
   margin: 0 auto;
@@ -14,7 +23,24 @@ const BaContainer = styled.div`
 function Banner() {
   return (
     <div>
-      <BaContainer>배너이미지</BaContainer>
+      <Swiper
+        modules={[Navigation, Pagination, Autoplay]}
+        spaceBetween={50}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        autoplay={{ delay: 5000 }}
+        loop={true}>
+        <SwiperSlide>
+          <BaContainer bg='gray'>배너이미지1</BaContainer>
+        </SwiperSlide>
+        <SwiperSlide>
+          <BaContainer bg='#9A5123'>배너이미지2</BaContainer>
+        </SwiperSlide>
+        <SwiperSlide>
+          <BaContainer bg='skyblue'>배너이미지3</BaContainer>
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 }
