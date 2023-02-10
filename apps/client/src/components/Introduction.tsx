8b5d1f503f3835cfe7966e208f56acba53c907ee
kimhsr <kimhsr@naver.com>
2023-02-11 02:02:12 +0900

    fix: 사이트 소개 수정

--- apps/client/src/components/Introduction.tsx
+++ apps/client/src/components/Introduction.tsx
@@ -2,25 +2,41 @@ import React from 'react';
 import styled from 'styled-components';
 
 const InContainer = styled.div`
-  background-color: #e9ebec;
+  background-color: #fff;
   width: 100%;
   max-width: 71.25rem;
   margin: 0 auto;
-  height: 150px;
-  position: relative;
+  height: 180px;
+  text-align: center;
 `;
 
 const InText = styled.div`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+  line-height: 5.8rem;
+  font-size: 1.5rem;
+  color: #383838;
+`;
+
+const InButton = styled.div`
+  display: inline-block;
+  padding: 1rem 2rem;
+  border: none;
+  border-radius: 10px;
+  background-color: #383838;
+  text-decoration: none;
+  color: #fff;
+  line-height: 1.5;
+  cursor: pointer;
+  transform: rotate(0.03deg);
+  &:hover {
+    background-color: #000;
+  }
 `;
 
 function Introduction() {
   return (
     <InContainer>
-      <InText>구인구직 사이트 잡다입니다.</InText>
+      <InText>구인구직 사이트 잡다입니다</InText>
+      <InButton>채용공고</InButton>
     </InContainer>
   );
 }
