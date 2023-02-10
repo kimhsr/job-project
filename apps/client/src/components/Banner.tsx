2f9872c5996656f43822cf7734dc7f16aef12b0c
Kim Hyun Sung <kimhsr@naver.com>
2023-02-11 01:00:03 +0900

    fix: max-witdh 수정 (#8)

--- apps/client/src/components/Banner.tsx
+++ apps/client/src/components/Banner.tsx
@@ -11,37 +11,42 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 
-const BaContainer = styled.div<{ bg: string }>`
-  position: relative;
-  top: 25px;
-  background-color: ${(props) => props.bg};
+const BannerContainer = styled.div`
   width: 100%;
   max-width: 71.25rem;
   margin: 0 auto;
+`;
+
+const BannerImg = styled.div<{ bg: string }>`
+  position: relative;
+  top: 25px;
+  background-color: ${(props) => props.bg};
   height: 370px;
 `;
 
 function Banner() {
   return (
     <div>
-      <Swiper
-        modules={[Navigation, Pagination, Autoplay]}
-        spaceBetween={50}
-        slidesPerView={1}
-        navigation
-        pagination={{ clickable: true }}
-        autoplay={{ delay: 5000 }}
-        loop={true}>
-        <SwiperSlide>
-          <BaContainer bg='gray'>배너이미지1</BaContainer>
-        </SwiperSlide>
-        <SwiperSlide>
-          <BaContainer bg='#9A5123'>배너이미지2</BaContainer>
-        </SwiperSlide>
-        <SwiperSlide>
-          <BaContainer bg='skyblue'>배너이미지3</BaContainer>
-        </SwiperSlide>
-      </Swiper>
+      <BannerContainer>
+        <Swiper
+          modules={[Navigation, Pagination, Autoplay]}
+          spaceBetween={50}
+          slidesPerView={1}
+          navigation
+          pagination={{ clickable: true }}
+          autoplay={{ delay: 5000 }}
+          loop={true}>
+          <SwiperSlide>
+            <BannerImg bg='gray'>배너이미지1</BannerImg>
+          </SwiperSlide>
+          <SwiperSlide>
+            <BannerImg bg='#9A5123'>배너이미지2</BannerImg>
+          </SwiperSlide>
+          <SwiperSlide>
+            <BannerImg bg='skyblue'>배너이미지3</BannerImg>
+          </SwiperSlide>
+        </Swiper>
+      </BannerContainer>
     </div>
   );
 }
