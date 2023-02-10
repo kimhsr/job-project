fac4b65739ecbb56a9285ab045c6b0d5834577da
kimhsr <kimhsr@naver.com>
2023-02-10 23:38:51 +0900

    fix: max-width 수정

--- apps/client/src/components/Banner.tsx
+++ apps/client/src/components/Banner.tsx
@@ -11,37 +11,42 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 
-const BaContainer = styled.div<{ bg: string }>`
-  position: relative;
-  top: 25px;
-  background-color: ${(props) => props.bg};
-  width: 100%;
+const BaContainer = styled.div`
   max-width: 71.25rem;
   margin: 0 auto;
+  width: 100%;
+  position: relative;
+  top: 25px;
+`;
+
+const BaImg = styled.div<{ bg: string }>`
+  background-color: ${(props) => props.bg};
   height: 370px;
 `;
 
 function Banner() {
   return (
     <div>
-      <Swiper
-        modules={[Navigation, Pagination, Autoplay]}
-        spaceBetween={50}
-        slidesPerView={1}
-        navigation
-        pagination={{ clickable: true }}
-        autoplay={{ delay: 5000 }}
-        loop={true}>
-        <SwiperSlide>
-          <BaContainer bg='gray'>배너이미지1</BaContainer>
-        </SwiperSlide>
-        <SwiperSlide>
-          <BaContainer bg='#9A5123'>배너이미지2</BaContainer>
-        </SwiperSlide>
-        <SwiperSlide>
-          <BaContainer bg='skyblue'>배너이미지3</BaContainer>
-        </SwiperSlide>
-      </Swiper>
+      <BaContainer>
+        <Swiper
+          modules={[Navigation, Pagination, Autoplay]}
+          spaceBetween={50}
+          slidesPerView={1}
+          navigation
+          pagination={{ clickable: true }}
+          autoplay={{ delay: 5000 }}
+          loop={true}>
+          <SwiperSlide>
+            <BaImg bg='gray'>배너이미지1</BaImg>
+          </SwiperSlide>
+          <SwiperSlide>
+            <BaImg bg='#9A5123'>배너이미지2</BaImg>
+          </SwiperSlide>
+          <SwiperSlide>
+            <BaImg bg='skyblue'>배너이미지3</BaImg>
+          </SwiperSlide>
+        </Swiper>
+      </BaContainer>
     </div>
   );
 }
