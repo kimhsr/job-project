efbb6d2f5475674aee6205c2bd16ef356bfa09a1
kimhsr <kimhsr@naver.com>
2023-02-11 19:42:55 +0900

    fix: fotter css 수정

--- apps/client/src/components/Footer.tsx
+++ apps/client/src/components/Footer.tsx
@@ -3,13 +3,34 @@ import styled from 'styled-components';
 
 const FooContainer = styled.div`
   width: 100%;
-  background-color: black;
-  border-bottom: 1px solid #e5e5e5;
-  height: 70px;
+  background-color: white;
+  border-top: 1px solid #e5e5e5;
+`;
+const FooButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100px;
+`;
+
+const FooButton = styled.div`
+  background-color: #383838;
+  border-radius: 30px;
+  width: 60px;
+  height: 60px;
+  margin: 30px;
 `;
 
 function Footer() {
-  return <FooContainer></FooContainer>;
+  return (
+    <FooContainer>
+      <FooButtonContainer>
+        <FooButton></FooButton>
+        <FooButton></FooButton>
+        <FooButton></FooButton>
+      </FooButtonContainer>
+    </FooContainer>
+  );
 }
 
 export default Footer;
