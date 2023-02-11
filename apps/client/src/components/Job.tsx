3037e93db88c3f034edf33bc6662878f0a013151
kimhsr <kimhsr@naver.com>
2023-02-11 16:13:42 +0900

    feat: job Grid 작업

--- apps/client/src/components/Job.tsx
+++ apps/client/src/components/Job.tsx
@@ -2,15 +2,47 @@ import React from 'react';
 import styled from 'styled-components';
 
 const JobContainer = styled.div`
-  background-color: skyblue;
+  border-top: 1px solid #e5e5e5;
+  padding-top: 25px;
+  padding-bottom: 25px;
+  background-color: white;
   width: 100%;
   max-width: 71.25rem;
   margin: 0 auto;
-  height: 400px;
+  height: 700px;
+`;
+
+const GridContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr;
+  width: 100%;
+  height: 100%;
+  background-color: white;
+  gap: 20px;
+`;
+
+const GridItem = styled.div`
+  background-color: white;
+  border: 1px solid #e5e5e5;
 `;
 
 function Job() {
-  return <JobContainer>구인구직</JobContainer>;
+  return (
+    <JobContainer>
+      <GridContainer>
+        <GridItem>채용공고</GridItem>
+        <GridItem>채용공고</GridItem>
+        <GridItem>채용공고</GridItem>
+        <GridItem>채용공고</GridItem>
+        <GridItem>채용공고</GridItem>
+        <GridItem>채용공고</GridItem>
+        <GridItem>채용공고</GridItem>
+        <GridItem>채용공고</GridItem>
+        <GridItem>채용공고</GridItem>
+      </GridContainer>
+    </JobContainer>
+  );
 }
 
 export default Job;
