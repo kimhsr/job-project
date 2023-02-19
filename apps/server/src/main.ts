1fb6e3ea1344b2610f14ed9545cbb154af80a202
nawhes <nawhes@naver.com>
2023-02-20 03:33:05 +0900

    Enable cors

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -7,6 +7,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  app.enableCors({
+    origin: 'https://jobda.nawhes.dev',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    credentials: true,
+  });
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
